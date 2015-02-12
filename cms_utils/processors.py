7054aecd75e2851212963cb42fa42b765c539963
David Mugnai <dvd@develer.com>
2015-02-12 16:19:03 +0100

    template processor fixed to work with markitup content
    templates are compiled and render on the raw body

--- cms_utils/processors.py
+++ cms_utils/processors.py
@@ -1,14 +1,26 @@
 # -*- coding: utf-8 -*-
+from .models import MarkitUpPluginModel
 from django.template import Template
-
+from django.utils.safestring import mark_safe
+from markitup.fields import render_func
 
 def process_templatetags(instance, placeholder, rendered_content, original_context):
     """
     This plugin processor render the resulting content to parse for templatetags
     in the plugin output
     """
-    try:
-        template = Template(rendered_content)
-    except Exception, e:
-        return u'<p><strong>Template Error: {}</strong></p>{}'.format(str(e), rendered_content)
-    return template.render(original_context)
+    def render(tpl_source):
+        try:
+            template = Template(tpl_source)
+        except Exception, e:
+            return u'<p><strong>Template Error: {}</strong></p>{}'.format(str(e), rendered_content)
+        return template.render(original_context)
+
+    if isinstance(instance, MarkitUpPluginModel):
+        # this is a markitup field, rendered_content is not anymore a valid
+        # django template. I have to work on the raw content.
+        mark = render(instance.body.raw)
+        html = mark_safe(render_func(mark))
+    else:
+        html = render(rendered_content)
+    return html
