2e071c0e37fac657955de70fb7193b3e46ba2aef
Carlo Miron <C8E@users.noreply.github.com>
2017-12-27 22:17:14 +0100

    Update subscribe_speakers_to_talks.py

--- p3/management/commands/subscribe_speakers_to_talks.py
+++ p3/management/commands/subscribe_speakers_to_talks.py
@@ -1,7 +1,10 @@
 # -*- coding: UTF-8 -*-
 from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth import get_user_model
 from conference import models as cmodels
 from hcomments import models as hmodels
+
+info = get_user_model().objects.get(email='info@pycon.it')
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
@@ -17,3 +20,4 @@ class Command(BaseCommand):
             t = row.talk
             print '%s %s -> %s' % (u.first_name, u.last_name, t.title)
             hmodels.ThreadSubscription.objects.subscribe(t, u)
+            hmodels.ThreadSubscription.objects.subscribe(t, info)
