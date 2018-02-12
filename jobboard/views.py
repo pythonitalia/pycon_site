835c87a6f6ea46bf2153d122cbfb1ea58bedd180
Ernesto Arbitrio <ernesto.arbitrio@gmail.com>
2018-02-12 16:37:40 +0100

    overrided get_queryset method for job list to order posts by pk, fixed list template avoiding bothering stuff with safe filter and html tags in description preview

--- jobboard/views.py
+++ jobboard/views.py
@@ -7,9 +7,13 @@ from django.shortcuts import HttpResponse
 class JobList(ListView):
     model = JobOffer
 
+    def get_queryset(self):
+        return JobOffer.objects.order_by('pk')
+
     def get_context_data(self, **kwargs):
         context = super(JobList, self).get_context_data(**kwargs)
         return context
+
 
 class JobDetail(DetailView):
     model = JobOffer
