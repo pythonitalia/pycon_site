27f630c856e77433fd1f7e58bdfdf2a16179bd9a
Ernesto Arbitrio <ernesto.arbitrio@gmail.com>
2019-03-01 18:07:57 +0100

    bug fixing on cert of attendance

--- p3/templatetags/p3.py
+++ p3/templatetags/p3.py
@@ -658,7 +658,9 @@ def p3_voting_data(conference):
 @fancy_tag(register, takes_context=True)
 def get_latest_conf_deadline(context, limit=None, not_expired=True):
     try:
-        conf = ConferenceModels.Conference.objects.latest('code')
+        conf = ConferenceModels.Conference.objects.latest('conference_end')
+        print(conf)
+        print(conf.name, conf.code, conf.conference_start, conf.conference_end, datetime.today().date())
         return [conf.name, conf.code, conf.conference_start, conf.conference_end, datetime.today().date()]
     except IndexError:
         return []
